a69f17a81ee79a3cae137d7b6f202239fd568be0
aline-ayrao <alineayrao@gmail.com>
2022-11-15 16:21:58 -0300

    Exercicio Selenium

--- src/test/java/selenium/Exercicios_selenium.java
+++ src/test/java/selenium/Exercicios_selenium.java
@@ -1,7 +1,11 @@
 package selenium;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,11 +16,17 @@ import org.openqa.selenium.support.ui.Select;
 public class Exercicios_selenium {
     WebDriver driver;
 
+    @Before
+    public void before()throws InterruptedException{
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10000,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://demo.automationtesting.in/Register.html");
+    }
+
     @Test
     public void testeFromularioSelenium() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demo.automationtesting.in/Register.html");
-        Thread.sleep(3000);
+
         driver.findElement(By.xpath("//*[@placeholder='First Name']")).sendKeys("Aline"); // primeiro nome
         driver.findElement(By.xpath("//*[@placeholder='Last Name']")).sendKeys("Ayrão"); // ultimo nome
         driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[2]/div/textarea"))
@@ -26,7 +36,6 @@ public class Exercicios_selenium {
         driver.findElement(By.xpath("//*[@ng-model='radiovalue' and @value='FeMale']")).click(); // seleciona o radiobuton da tela
         // driver.quit(); //fecha o navegador e encerra o processo
         // driver.close(); //fecha o navegador se tiver uma aba só aberta ou a aba e não mata o processo
-
         Assert.assertTrue(driver.findElement(By.xpath("//*[@ng-model='radiovalue' and @value='FeMale']")).isSelected());
         // Assert.assertFalse("Elemento não selecionado",driver.findElement(By.xpath("//*[@ng-model='radiovalue' and
         // @value='FeMale']")).isSelected());// validar se não está criando um teste fake, msg será retornada no erro.
@@ -36,10 +45,8 @@ public class Exercicios_selenium {
                 driver.findElement(By.xpath("//input[@value='Movies']")).isSelected());
 
         driver.findElement(By.xpath("//div[@id='msdd']")).click();// clica na lista
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//a[contains(text(), 'Portuguese')]")).click(); // seleciona opção da lista
-        driver.findElement(By.xpath("//a[contains(text(), 'English')]")).click(); // seleciona mais de uma opção da lista
-                                                                                  
+        driver.findElement(By.xpath("//a[contains(text(), 'English')]")).click(); // seleciona mais de uma opção da lista                                                          
         boolean linguagemPortugueseSelecionada = driver
                 .findElement(
                         By.xpath("//div[contains(text(), 'Portuguese') and @class='ui-autocomple-multiselect-item"))
@@ -68,13 +75,12 @@ public class Exercicios_selenium {
 
         driver.findElement(By.xpath("//*[@id='firstpassword']")).sendKeys("Teste");
         driver.findElement(By.xpath("//*[@id='secondpassword']")).sendKeys("Teste");
-        driver.findElement(By.xpath("//*[@id='submitbtn']")).click();
+        driver.findElement(By.cssSelector("//*[@id='submitbtn']")).click();
 
     }
 
     @AfterClass
     public void after() throws InterruptedException {
-        Thread.sleep(3000);
         driver.quit();
     }
 }
