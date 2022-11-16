f51ccfd0cd9b453d2aa7b261ec6b0bc1f0d1a0f9
Aline Ayrão <alineayrao@gmail.com>
2022-11-16 19:06:32 -0300

    Primeira Execução

--- src/test/java/selenium/PrincipaisComandosSelenium.java
+++ src/test/java/selenium/PrincipaisComandosSelenium.java
@@ -10,23 +10,23 @@ public class PrincipaisComandosSelenium {
     public void testeSeleniumClick() throws InterruptedException{
         WebDriver driver = new ChromeDriver(); //abre o navegador
         driver.get("https://www.qaacademy.com.br"); //abre o site desejado
-        Thread.sleep(3000);//espera para não dar erro entre os comandos
-        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/botton[1]")).click(); //localiza o cookie e clica em aceitar
+        Thread.sleep(5000);//espera para não dar erro entre os comandos
+        driver.findElement(By.xpath("//html/body/div[2]/div/div/div[1]/button[1]")).click(); //localiza o cookie e clica em aceitar
         Thread.sleep(1000);//espera para não dar erro entre os comandos
-        driver.findElement(By.xpath("//*[@id='img_1_comp-k0wqferu']/img")).click(); //clica no botão do linkedin da pagina
+        driver.findElement(By.xpath("//*[@id='dataItem-k3hb7c5j-comp-k0wqferu']/a")).click(); //clica no botão do linkedin da pagina
     }
 
     @Test
     public void testeSeleniumEnviarTextoParaTela() throws InterruptedException{
         WebDriver driver = new ChromeDriver(); //abre o navegador
         driver.get("https://www.qaacademy.com.br"); //abre o site desejado
-        Thread.sleep(3000);//espera para não dar erro entre os comandos
-        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/botton[1]")).click(); //localiza o cookie e clica em aceitar
+        Thread.sleep(5000);//espera para não dar erro entre os comandos
+        driver.findElement(By.xpath("//html/body/div[2]/div/div/div[1]/button[1]")).click(); //localiza o cookie e clica em aceitar
         Thread.sleep(1000);//espera para não dar erro entre os comandos
-        driver.findElement(By.xpath("//*[@placeholder='Nome']")).click(); //clica no campo nome
-        driver.findElement(By.xpath("//*[@placeholder='Nome']")).sendKeys("Aline Ayrão");//envia o texo para o campo nome
-        driver.findElement(By.xpath("//*[@placeholder='Email']")).click(); //clica no campo E-mail
-        driver.findElement(By.xpath("//*[@placeholder='Email']")).sendKeys("alineayrao@gmail.com");//envia o texo para o campo e-mail
+        driver.findElement(By.xpath("//*[@id='input_comp-k37gdip8']")).click(); //clica no campo nome
+        driver.findElement(By.xpath("//*[@id='input_comp-k37gdip8']")).sendKeys("Aline Ayrão");//envia o texo para o campo nome
+        driver.findElement(By.xpath("//*[@id='input_comp-k37gdiq1']")).click(); //clica no campo E-mail
+        driver.findElement(By.xpath("//*[@id='input_comp-k37gdiq1']")).sendKeys("alineayrao@gmail.com");//envia o texo para o campo e-mail
         driver.findElement(By.xpath("//*[@id='comp-k37gdiro']/button")).click(); //clica em enviar
         
     }
